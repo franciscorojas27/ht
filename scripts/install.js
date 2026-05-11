43e332ea5dc8c3c4ef8abec6833849d288f07d16
Francisco Rojas <franciscoantonior30@gmail.com>
2026-05-11 11:44:54 -0400

    Fix: final attempt with explicit npmrc and correct bin extension

--- scripts/install.js
+++ scripts/install.js
@@ -4,7 +4,7 @@ const https = require('https');
 const path = require('path');
 
 
-const version = "0.1.5";
+const version = "0.1.6";
 const repo = "franciscorojas27/ht";
 
 const platform = os.platform() === 'win32' ? 'windows' : os.platform();
